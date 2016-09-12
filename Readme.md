fe22216b604c9b1e51d4b78de80df9b4acc644a0
bjorn3 <bjorn3@users.noreply.github.com>
2016-09-12 18:09:10 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Rust rfc viewer
+===============
+A viewer for the rust [rfcs](https://github.com/rust-lang/rfcs)
+
+Warning: this may crash your browser, because every rfc is rendered which could be too many elements.
+
+## Todo:
+[ ] Add filters
+[ ] Cache rfcs
+[ ] Plug markdown renderer in
---
+++ app.js
@@ -0,0 +1,120 @@
+//jshint enforceall:true
+//jshint camelcase:false
+//jshint browser:true
+//jshint esnext:true
+/* globals fetch */
+/* exported open_text_popup, open_url_popup, close_text_popup*/
+
+"use strict";
+
+let rfc_data = [];
+
+let template = `<li>
+    <span class="state {{state}}">{{state}}</span><span class="id">{{id}}</span><span class="name">{{name}}</span>
+    <span class="buttons">
+        <a href="#" data-file="{{text}}" onclick="open_text_popup(event)">Text</a>
+        <a href="{{discussion}}">Discussion</a>
+    </span>
+</li>`;
+
+window.onload = function(){
+    
+    fetch("https://api.github.com/repos/rust-lang/rfcs/contents/text").then((res)=>res.json())
+        .then(function(json){
+            console.log(json);
+            for(let rfc of json){
+                rfc_data.push({
+                    id: rfc.name.split("-")[0],
+                    name: rfc.name.split("-").slice(1).join(" ").replace(/\.md$/, ""),
+                    text: rfc.download_url,
+                    discussion: "about:blank",
+                    state: "Accepted"
+                });
+            }
+            show_rfcs();
+        });
+    
+    let next_pr_url = "https://api.github.com/repos/rust-lang/rfcs/pulls?state=all";
+    function pr_status(url){
+        fetch(url)
+            .then(function(res){
+                console.log(res);
+                let prs_url = res.headers.get("Link").split(";")[0].replace(/^<(.*)>$/, "$1");
+                if(next_pr_url === prs_url){
+                    next_pr_url = null;
+                }else{
+                    next_pr_url = prs_url;
+                }
+                return res.json();
+            }).then(function(json){
+                console.log(json);
+                for(let pr of json){
+                    let state;
+                    if(pr.merged_at){
+                        state = "Accepted";
+                    }else if(pr.state === "open"){
+                        state = "Open";
+                    }else if(pr.state === "closed"){
+                        state = "Rejected";
+                    }else{
+                        console.log(pr.state);
+                    }
+                    rfc_data.push({
+                        id: pr.number,
+                        name: pr.title,
+                        text: pr.name,
+                        discussion: pr.html_url,
+                        state: state
+                    });
+                }
+                show_rfcs();
+            });
+    }
+    
+    let pr_status_interval = window.setInterval(function(){
+        if(next_pr_url){
+            pr_status(next_pr_url);
+        }else{
+            window.clearInterval(pr_status_interval);
+        }
+    }, 5000);
+    
+    show_rfcs();
+};
+
+function show_rfcs(){
+    let rfc_list = document.querySelector("#rfc_list");
+    rfc_data = rfc_data.sort(function(a,b){
+        return a.id > b.id;
+    });
+    rfc_list.innerHTML = "";
+    for(let rfc of rfc_data){
+        rfc_list.innerHTML += template
+            .replace("{{id}}", rfc.id)
+            .replace("{{name}}", rfc.name)
+            .replace("{{text}}", rfc.text)
+            .replace("{{discussion}}", rfc.discussion)
+            .replace(/{{state}}/g, rfc.state)
+        ;
+    }
+}
+
+function open_text_popup(evt){
+    evt.preventDefault();
+    fetch(evt.originalTarget.dataset.file)
+        .then(function(res){
+            return res.text();
+        }).then(function(text){
+            document.querySelector("#rfc_text").innerHTML = "<a href=\"#\" onclick=\"close_text_popup(event)\" class=\"close_button\">X</a>" + text;
+            document.querySelector("#rfc_text_wrapper").hidden = false;
+        });
+}
+
+function open_url_popup(evt){
+    document.querySelector("#rfc_text").innerHTML = `<a href="#" onclick=\"close_text_popup(event)\" class=\"close_button\">X</a><iframe src="` + evt.originalTarget.dataset["href"] +"\"></iframe>"; //jshint ignore:line
+    document.querySelector("#rfc_text_wrapper").hidden = false;
+}
+
+function close_text_popup(){
+    document.querySelector("#rfc_text_wrapper").hidden = true;
+}
